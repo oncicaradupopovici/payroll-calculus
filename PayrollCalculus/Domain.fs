c45409ae10da2382a69320b2e2fb774053684533
Radu Popovici <rpopovici@totalsoft.ro>
2020-02-18 13:17:33 +0200

    parser and data effects

--- PayrollCalculus/Domain.fs
+++ PayrollCalculus/Domain.fs
@@ -22,19 +22,6 @@ module Effect =
     
         List.foldBack folder list initState
 
-
-//type StateResult<'s, 't> = StateResult of ('s -> Result<'t * 's, string>)
-//module StateResult =
-//    let run (StateResult x) : 's -> Result<'t * 's, string> = 
-//        x 
-//    let map (f: 't->'u) (m : StateResult<'s, 't>) : StateResult<'s,'u> = 
-//        StateResult (fun s -> Result.map (fun (a, s') -> (f a, s')) (run m s)) 
-//    let bind (f: 't-> StateResult<'s, 'u>) (m : StateResult<'s, 't>) : StateResult<'s, 'u> = 
-//        StateResult (fun s -> Result.bind (fun (a, s') -> run (f a) s') (run m s))
-//    let apply (StateResult f: StateResult<'s, ('t -> 'u)>) (m: StateResult<'s, 't>) : StateResult<'s, 'u> = 
-//        StateResult (fun s -> Result.bind (fun (g, s') -> Result.map (fun (a: 't, s'': 's) -> ((g a), s'')) (run m s')) (f s)) 
-
-
 type StateResult<'s, 't> = 's -> Result<'t * 's, string>
 module StateResult =
     let run (x: StateResult<'s, 't>) : 's -> Result<'t * 's, string> = 
@@ -48,32 +35,37 @@ module StateResult =
 
     let retn x = fun s -> Result.Ok (x, s)
 
+    let traverseE f list =
+        let cons head tail = head :: tail  
+        let initState = Evented.pure' []
+        let folder head tail = Evented.pure' cons <*> (f head) <*> tail
+        List.foldBack folder list initState
 
-module Domain = 
-    type ElemCode = ElemCode of string
 
-    type ElemType = 
-        | DataAccess of table:string * column:string
-        | Formula of formula:string * deps: string list
+module Domain =
 
     type ElemDefinition = {
         Code: ElemCode
         Type: ElemType
     }
-    
-    type PersonId = PersonId of Guid
+    and ElemCode = ElemCode of string
+    and ElemType = 
+        | Db of DbElemDefinition
+        | Formula of FormulaElemDefinition
+    and DbElemDefinition = {table:string; column:string}
+    and FormulaElemDefinition = {formula:string; deps: string list}
 
-    type YearMonth = {
-        Year: int
-        Month: int
-    }
 
-    type ComputationCtx = {
+    type Elem<'T> = Elem of (ComputationCtx -> IEffect<'T>)
+    and ComputationCtx = {
         PersonId: PersonId
         YearMonth: YearMonth
     }
-
-    type Elem<'T> = Elem of (ComputationCtx -> IEffect<'T>)
+    and YearMonth = {
+        Year: int
+        Month: int
+    }
+    and PersonId = PersonId of Guid
 
     module Elem = 
         let liftDelegate (deleg: Delegate) = 
@@ -85,69 +77,98 @@ module Domain =
                             |> Effect.sequenceList 
                             |> Effect.map (List.toArray >> (fun arr' -> deleg.DynamicInvoke arr'))
                     )
-                
 
+        let flattenEffect<'T> (x:IEffect<Elem<'T>>) = 
+            Elem (fun ctx -> Effect.bind x (fun (Elem fn) -> fn ctx))
 
-                        
-                
-            
+        let map (f: 'a-> 'b) ((Elem fn) : Elem<'a>) : Elem<'b> = 
+            Elem(fn >> Effect.map f)
 
-    type ElemDefinitionCache = Map<ElemCode, ElemDefinition>
-
-    type ElemCache = Map<ElemCode, Elem<obj>>
+        let bind (f: 'a-> Elem<'b>) ((Elem fn) : Elem<'a>) : Elem<'b> = 
+            Elem(fun ctx -> fn ctx >>= 
+                    (fun a -> 
+                        let (Elem fn') = f a
+                        fn' ctx
+                    )
+                )
 
     type ElemValuesCache = Map<ElemCode, obj>
 
-    type ProcessElemResult = StateResult<ElemCache, Elem<obj>>
 
-    type ProcessElem  = ElemDefinitionCache -> ElemCode -> ProcessElemResult
+    module ElemValueRepo = 
+        type LoadSideEffect = {
+            definition: DbElemDefinition
+            ctx: ComputationCtx
+        }
+        with interface ISideEffect<obj>
 
+        let load definition =  Elem(fun ctx -> Effect.Of {definition=definition; ctx=ctx})
 
+    module Parser =
+        type ParseFormulaSideEffect = {
+            formula:string
+        }
+        with interface ISideEffect<ParseFormulaResult>
+        and ParseFormulaResult = {
+            func: Delegate
+            parameters: string list
+        }
 
-     
+        let parseFormula formula = Effect.Of {formula=formula}
+
     
 
+    type ParseElemDefinition  = ElemDefinitionCache -> ElemCode -> ParseElemDefinitionResult
+    and ParseElemDefinitionResult = Elem<StateResult<ElemCache, obj>>
+    and ElemDefinitionCache = Map<ElemCode, ElemDefinition>
+    and ElemCache = Map<ElemCode, Elem<obj>>
 
-    let rec processElem: ProcessElem = 
+    let rec parseElemDefinition: ParseElemDefinition =
         fun elemDefinitionCache elemCode ->
             let elemDefinition = elemDefinitionCache.TryFind elemCode
 
-            let processDataAccess table column : ProcessElemResult = 
-                let elem = Elem (fun computationCtx -> Effect.pureEffect (Object()))
-                fun (s:ElemCache) -> Result.Ok(elem, s.Add (elemCode,elem))
-            
-
-            let processFormula formula : ProcessElemResult =
-                let parser = LambdaParser()
-                let expression = parser.Parse formula
-                let parameters = LambdaParser.GetExpressionParameters expression
-                let lambdaExpression = Expression.Lambda(expression, parameters)
-                let compiled = lambdaExpression.Compile();
-                let liftedDelegate = Elem.liftDelegate compiled
-
-                
+            let parseFormulaResultToElem  (result:Parser.ParseFormulaResult) =
+                let liftedDelegate = Elem.liftDelegate result.func
 
                 let results = 
-                    parameters|> Array.map (fun p -> processElem elemDefinitionCache (ElemCode p.Name))
-
+                    result.parameters|> Array.map (fun p -> parseElemDefinition elemDefinitionCache (ElemCode p))
+                
                 let folder =
-                    fun (current: ProcessElemResult) (acc: StateResult<ElemCache, Elem<obj> list>) ->
-                        acc |> StateResult.bind (fun elems -> current |> StateResult.map (fun elem -> elem :: elems))
-
+                    fun (current: Elem<StateResult<ElemCache, obj>>) (acc: Elem<StateResult<ElemCache, obj list>>) ->
+                        Elem.bind (StateResult.bind (fun elems -> current |> Elem.map (StateResult.map (fun elem -> elem :: elems))) current
+                
                 let results' = Array.foldBack folder results (StateResult.retn [])
-
+                
                 results' |> StateResult.bind 
                     (fun paramElems -> 
                         let resultElem = paramElems |> List.toArray |> liftedDelegate
                         (fun s -> Result.Ok(resultElem, s.Add (elemCode,resultElem))))
+
+
+            
+            let parseFormula {formula=formula} : ParseElemDefinitionResult =
+                let parserResult = 
+                    formula 
+                        |> Parser.parseFormula
+                        //|> Effect.map parseFormulaResultToElem
+                        |> Effect.map 
+                            (fun result ->
+                                let liftedDelegate = Elem.liftDelegate result.func
+                                let results = 
+                                    result.parameters
+                                        |> Array.map (fun p -> parseElemDefinition elemDefinitionCache (ElemCode p))
+                            )
+                                
+
+                
 
             
             match elemDefinition with
                 | None -> (fun _-> Result.Error "could not find definition")
                 | Some elemDefinition -> 
                     match elemDefinition.Type with
-                    | DataAccess(table,column) -> processDataAccess table column
-                    | Formula(formula, _) -> processFormula formula
+                    | Db(dbElemDefinition) -> ElemValueRepo.load dbElemDefinition
+                    | Formula(formulaElemDefinition) -> processFormula formula
 
             
 

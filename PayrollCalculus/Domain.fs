738c5c7f15d1342326328b3c4a411917fdd96770
Radu Popovici <rpopovici@totalsoft.ro>
2020-02-19 17:29:44 +0200

    StateEffect transformer wip

--- PayrollCalculus/Domain.fs
+++ PayrollCalculus/Domain.fs
@@ -4,19 +4,37 @@ open System
 open NBB.Core.Effects.FSharp
 open NBB.Core.Effects
 
-//type StateResult<'s, 't> = 's -> Result<'t * 's, string>
-//module StateResult =
-//    let run (x: StateResult<'s, 't>) : 's -> Result<'t * 's, string> = 
-//        x 
-//    let map (f: 't->'u) (m : StateResult<'s, 't>) : StateResult<'s,'u> = 
-//        fun s -> Result.map (fun (a, s') -> (f a, s')) (run m s)
-//    let bind (f: 't-> StateResult<'s, 'u>) (m : StateResult<'s, 't>) : StateResult<'s, 'u> = 
-//        fun s -> Result.bind (fun (a, s') -> run (f a) s') (run m s)
-//    let apply (f: StateResult<'s, ('t -> 'u)>) (m: StateResult<'s, 't>) : StateResult<'s, 'u> = 
-//        fun s -> Result.bind (fun (g, s') -> Result.map (fun (a: 't, s'': 's) -> ((g a), s'')) (run m s')) (f s)
+type StateEffect<'s, 't> = 's -> IEffect<'t * 's>
+module StateResult =
+    let run (x: StateResult<'s, 't>) : 's -> Result<'t * 's, string> = 
+        x 
+    let map (f: 't->'u) (m : StateResult<'s, 't>) : StateResult<'s,'u> = 
+        fun s -> Result.map (fun (a, s') -> (f a, s')) (run m s)
+    let bind (f: 't-> StateResult<'s, 'u>) (m : StateResult<'s, 't>) : StateResult<'s, 'u> = 
+        fun s -> Result.bind (fun (a, s') -> run (f a) s') (run m s)
+    let apply (f: StateResult<'s, ('t -> 'u)>) (m: StateResult<'s, 't>) : StateResult<'s, 'u> = 
+        fun s -> Result.bind (fun (g, s') -> Result.map (fun (a: 't, s'': 's) -> ((g a), s'')) (run m s')) (f s)
 
-//    let retn x = fun s -> Result.Ok (x, s)
+    let retn x = fun s -> Result.Ok (x, s)
 
+
+type State<'s, 't> = 's -> 't * 's
+module State =
+    let run (x: State<'s, 't>) : 's -> 't * 's = 
+        x 
+    let map (f: 't->'u) (m : State<'s, 't>) : State<'s,'u> = 
+        fun s -> let (a, s') = run m s in (f a, s')
+    let bind (f: 't-> State<'s, 'u>) (m : State<'s, 't>) : State<'s, 'u> = 
+        fun s -> let (a, s') = run m s in run (f a) s'
+    let apply (f: State<'s, ('t -> 'u)>) (m: State<'s, 't>) : State<'s, 'u> = 
+        fun s -> let (f, s') = run f s in let (a, s'') = run m s' in (f a, s'')
+
+    let get : State<'s, 's> = 
+        fun s -> (s, s)   
+    let put (x: 's) : State<'s, unit> = 
+        fun _ -> ((), x)
+
+    let pure' x = fun s -> (x, s)
 
 module List =
     let traverseResult f list =
@@ -28,9 +46,16 @@ module List =
         List.foldBack folder list initState
 
     let sequenceResult list = traverseResult id list
-    
 
+    let traverseState f list =
+        let pure' = State.pure'
+        let (<*>) = State.apply
+        let cons head tail = head :: tail  
+        let initState = pure' []
+        let folder head tail = pure' cons <*> (f head) <*> tail
+        List.foldBack folder list initState
 
+    let sequenceState list = traverseState id list
    
 
 
@@ -79,6 +104,8 @@ module Domain =
                     
     type ElemValuesCache = Map<ElemCode, obj>
 
+    type ElemCache = Map<ElemCode,Elem<obj>>
+
     module Result = 
         let traverseElem (f: 'a-> Elem<'c>) (result:Result<'a,'b>) : Elem<Result<'c, 'b>> = 
             let pure' x = fun ctx -> Effect.pure' x
@@ -96,6 +123,13 @@ module Domain =
                 |Ok v -> Effect.map Result.Ok (f v)
 
         let sequenceEffect result = traverseEffect id result
+
+        let traverseState (f: 'a-> State<'s, 'b>) (result:Result<'a,'e>) : State<'s, Result<'b, 'e>> = 
+            match result with
+                |Error err -> State.map Result.Error (State.pure' err)
+                |Ok v -> State.map Result.Ok (f v)
+
+        let sequenceState result = traverseState id result
 
 
     module ElemValueRepo = 
@@ -176,16 +210,51 @@ module Domain =
                 |> Result.traverseEffect matchElemDefinition
                 |> Effect.map (Result.sequenceElem >> Elem.flattenResult)
 
+
+    type ComputeElem3  = ElemDefinitionCache -> ElemCache -> ElemCode -> IEffect<Elem<obj>*ElemCache>
+    let rec computeElem3: ComputeElem3 =
+        fun elemDefinitionCache elemCode -> 
+
+            let computeFormula {formula=formula} : ElemCache -> IEffect<Elem<obj>*ElemCache> =
+                fun elemCache -> 
+                    formula
+                        |> Parser.parseFormula
+                        |> Effect.bind
+                            (fun {func=func;parameters=parameters} ->
+                                let state = 
+                                    parameters
+                                        |> List.traverseEffect (ElemCode >> computeElem3 elemDefinitionCache elemCache)
+                                        |> State.map (List.sequenceEffect >> Effect.map (List.toArray >> Elem.liftFunc func))
+                                let (eff, elemCache') = State.run state elemCache
+                                eff |> Effect.map (fun x-> (x, elemCache'))
+                                    //|> Effect.map (List.sequenceState >> State.map (List.toArray >> Elem.liftFunc func))
+                            )
+
+                
+            let matchElemDefinition elemDefinition = 
+                match elemDefinition.Type with
+                | Db(dbElemDefinition) -> dbElemDefinition |> ElemValueRepo.load |> State.pure'|> Effect.pure'
+                | Formula(formulaElemDefinition) -> computeFormula formulaElemDefinition
+
+
+            elemCode
+                |> ElemDefinitionCache.findElemDefinition elemDefinitionCache
+                |> Result.traverseEffect matchElemDefinition
+                |> Effect.map (Result.sequenceState >> State.map Result.sequenceElem >> State.map Elem.flattenResult)
+                |> Effect.map (State.bind (fun elem cache -> (elem, cache.Add(elemCode, elem))))
+
+
    
             
 
     let loadElemDefinitions() = Effect.pure' Map.empty<ElemCode, ElemDefinition>
 
-    let h elemCode ctx = 
+    let h elemCode1 elemCode2 ctx = 
         effect{
             let! elemDefinitionCache = loadElemDefinitions()
-            let! elem = computeElem2 elemDefinitionCache elemCode
-            let! value = elem ctx
+            let! (elem1,cache) = computeElem3 elemDefinitionCache elemCode1 Map.empty<ElemCode, Elem<obj>>
+            let! (elem2,cache) = computeElem3 elemDefinitionCache elemCode2 cache
+            let! value = elem2 ctx
             return value
         }
                 
